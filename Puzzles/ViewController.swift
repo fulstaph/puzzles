30c3c739f65e6136d760b1d382d22555aca6206a
Leonid Serebryanyy <leosilver@yandex.ru>
2019-11-18 23:13:20 +0300

    Initial Commit

---
+++ Puzzles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Puzzles
+//
+//  Created by Leonid Serebryanyy on 18.11.2019.
+//  Copyright © 2019 Leonid Serebryanyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
